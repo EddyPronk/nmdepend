1436d141b976744266509f0222b6cdc8690a36b0
epronk <epronk>
2005-06-23 20:02:08 +0000

    *** empty log message ***

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -1,57 +1,39 @@
-IF(FALSE)
-
-FIND_PATH(Boost_ROOT_DIR include/boost-1_32/boost/utility.hpp 
-		    ${Boost_ROOT_DIR} 
-		    /usr/include
-		    /usr/local/include
-		    /opt
-		    /opt/include
-		    DOC "Path to Boost directory"
+FIND_PATH(Boost_INCLUDE_DIR boost/config.hpp
+	/usr/include
+	/usr/local/include/boost-1_32/
 	)
 
-#FIND_PATH(Boost_INCLUDE_DIR /usr/include/boost/config.hpp
-
-ENDIF(FALSE)
-
-SET(Boost_VER 1_31)
-#SET(Boost_INCLUDE_DIR ${Boost_ROOT_DIR}/include/boost-1_32)
-	    
 SET(Boost_LIB_SUFFIX "gcc")
 
+MACRO(BOOST_USE name)
+  MESSAGE("-- Use ${name}")
+  FIND_LIBRARY(Boost_${name}_LIBRARY 
+    NAMES boost_${name}
+          boost_${name}-${Boost_LIB_SUFFIX}
+          boost_${name}-${Boost_LIB_SUFFIX}-${Boost_VER}
+    PATHS ${Boost_ROOT_DIR}/lib
+          ${Boost_ROOT_DIR}/stage/lib
+	      /usr/local/lib
+	)
 
-  IF(NOT HAVE_BOOST_LIBRARIES)
-    EXEC_PROGRAM(${CMAKE_C_COMPILER}
-      ARGS -E \"${CMAKE_SOURCE_DIR}/cmake/CheckBoostVersion.cpp\"
-      OUTPUT_VARIABLE CMAKE_COMPILER_OUTPUT
-      RETURN_VALUE CMAKE_COMPILER_RETURN
-    )
+ENDMACRO(BOOST_USE)
 
-    STRING(REGEX MATCH "version=\"[0-9_]+\"" OUTP "${CMAKE_COMPILER_OUTPUT}")
-    STRING(REGEX MATCH "[0-9_]+" BOOST_VERSION "${OUTP}")
-    SET(HAVE_BOOST_LIBRARIES 1)
-    MESSAGE("Boost version ${BOOST_VERSION}")
-  ENDIF(NOT HAVE_BOOST_LIBRARIES)
+#BOOST_USE(regex)
 
+#SET(Boost_VER 1_31)
+#SET(Boost_INCLUDE_DIR ${Boost_ROOT_DIR}/include/boost-1_32)
+	    
+IF(NOT HAVE_BOOST_LIBRARIES)
+	EXEC_PROGRAM(${CMAKE_C_COMPILER}
+    ARGS -E \"${CMAKE_SOURCE_DIR}/cmake/CheckBoostVersion.cpp\"
+	OUTPUT_VARIABLE CMAKE_COMPILER_OUTPUT
+  	RETURN_VALUE CMAKE_COMPILER_RETURN
+	)
 
-FIND_LIBRARY(Boost_Regex_LIBRARY 
-  NAMES boost_regex-${Boost_LIB_SUFFIX}-${Boost_VER}
-        boost_regex-${Boost_LIB_SUFFIX}
-  PATHS ${Boost_ROOT_DIR}/lib
-        ${Boost_ROOT_DIR}/stage/lib
-	/usr/lib
-  DOC "Boost Regular expressions")
+	STRING(REGEX MATCH "version=\"[0-9_]+\"" OUTP "${CMAKE_COMPILER_OUTPUT}")
+	STRING(REGEX MATCH "[0-9_]+" BOOST_VERSION "${OUTP}")
+	SET(HAVE_BOOST_LIBRARIES 1)
+	# MESSAGE("Boost version ${BOOST_VERSION}")
+ENDIF(NOT HAVE_BOOST_LIBRARIES)
 
-FIND_LIBRARY(Boost_Filesystem_LIBRARY
-  NAMES boost_filesystem-${Boost_LIB_SUFFIX}-${Boost_VER}
-        boost_filesystem-${Boost_LIB_SUFFIX}
-  PATHS ${Boost_ROOT_DIR}/lib
-        ${Boost_ROOT_DIR}/stage/lib
-	/usr/lib
-  DOC "Boost Filesystem")
-
-FIND_LIBRARY(Boost_program_options_LIBRARY
-  NAMES boost_program_options-gcc 
-  PATHS ${Boost_ROOT_DIR}/lib
-        ${Boost_ROOT_DIR}/stage/lib
-	/usr/lib
-  DOC "Boost Filesystem")
+  
